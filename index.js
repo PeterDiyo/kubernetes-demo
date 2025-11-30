fb7e5c49d36e8c9b7db2788c2d7e337b79937e0e
PeterDiyo <princediyo93@gmail.com>
2025-11-30 22:57:35 +0200

    Update index.js file

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.json({message: 'Hello, from a container',
-        servie: 'Hello node',
+        servie: 'Hello-node',
         pod: process.env.POD_NAME  || 'unknown',
         time: new Date().toISOString()
     });
 });
 
 app.get('/readyz', (req, res) => res.status(200).send('ready'));
-app.get('/health', (req, res) => res.status(200).send('Ok'));
+app.get('/healthz', (req, res) => res.status(200).send('Ok'));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
